eaebdf72bf2eb863e6765cc795f0279f7a44dc81
no <no>
2024-06-06 12:01:02 -0700

    working plot

--- main.py
+++ main.py
@@ -4,6 +4,11 @@
 import serial
 import struct
 import argparse
+import threading
+
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from collections import deque
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--port', type=str, default='/dev/ttyACM0')
@@ -20,27 +25,51 @@ rx_struct_size = struct.calcsize(rx_struct_format)
 tx_struct_format = 'i'
 tx_struct_size = struct.calcsize(tx_struct_format)
 
+plot_len = 1000
+xs = deque(maxlen=plot_len)
+yset = deque(maxlen=plot_len)
+ymeas = deque(maxlen=plot_len)
+
 # Transmit
 data = (999,)
 packed_data = struct.pack(tx_struct_format, *data)
 ser.write(packed_data)
 
 # Receive
-try:
+def readSerial():
+    global ymeas, yset, xs
+
+    i = 0
+
     while True:
-        print("Reading serial port")
-
         data = ser.read(rx_struct_size)
-
-        print(data)
 
         if len(data) == rx_struct_size:
             unpacked_data = struct.unpack(rx_struct_format, data)
 
-            print(unpacked_data)
+            ymeas.append(unpacked_data[0])
+            yset.append(unpacked_data[1])
+            xs.append(i)
 
-except KeyboardInterrupt:
-    print("Exiting Program")
+            i+=1
 
-finally:
-    ser.close()
+rx_thread = threading.Thread(target=readSerial)
+rx_thread.daemon = True
+rx_thread.start()
+
+# Plot
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+
+def animate(i, xs, ymeas, yset):
+    ax1.clear()
+    ax1.plot(xs, ymeas, label="measured")
+    ax1.plot(xs, yset, "-.", label="setpoint")
+
+ani = animation.FuncAnimation(
+    fig=fig, func=animate, interval=0.5, fargs=(xs, ymeas, yset), save_count=10
+)
+plt.show()
+
+# Exit
+ser.close()
